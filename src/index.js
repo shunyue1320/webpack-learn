3c19e44dcfe48e806d91ed05bda45e3a1f865755
shunyue1320 <tsunyue1320@gmail.com>
2022-03-08 15:39:40 +0800

    webpack 打包不同引入格式后的源码

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// import(/* webpackChunkName: "hello" */'./hello').then(result => {
+//     console.log(result);
+// });
+let title = require("./title.js");
+console.log(title);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+
+module.exports = {
+  mode: "development",
+  devtool: false,
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: ""
+  },
+  module: {},
+  plugins: [
+    new CleanWebpackPlugin(), // 清除前次打包文件
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    })
+  ],
+  devServer: {}
+}
